a30de3b3391c80b306a30610d954dfde8ef5a08b
maickolporras <126031835+maickolporras@users.noreply.github.com>
2023-03-22 21:29:34 -0500

    se agregaron cambios 1

---
+++ components/index/list-group.php
@@ -0,0 +1,7 @@
+<ol class="list-group list-group-numbered">
+    <li class="list-group-item">An item</li>
+    <li class="list-group-item">A second item</li>
+    <li class="list-group-item">A third item</li>
+    <li class="list-group-item">A fourth item</li>
+    <li class="list-group-item">And a fifth one</li>
+</ol>
---
+++ partials/navbar.php
@@ -0,0 +1,52 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
+  <div class="container">
+    <a class="navbar-brand" href="index.php">
+      <i class="fa-solid fa-user-tie fa-fade"></i>
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">
+            <i class="fa-solid fa-house fa-beat"></i>INICIO</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">ESTUDIOS</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            ESPERIENCIAS LABORALES
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">
+            <i class="fa-sharp fa-regular fa-medal fa-beat"></i>HABILIDADES</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">
+            <i class="fa-regular fa-graduation-cap fa-beat"></i>CURSOS REALIZADOS</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">
+            <i class="fa-regular fa-at fa-beat"></i>CONTACTAME</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="BUSCAR" aria-label="Search">
+        <button class="btn btn-" type="submit">
+          <i class="fa-solid fa-magnifying-glass"></i>
+        </button>
+      </form>
+    </div>
+  </div>
+</nav>
